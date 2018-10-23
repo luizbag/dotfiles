d43fed182c5b5fec46a73b539df2aae26f177bc1
Luiz Gavioli <428691+luizbag@users.noreply.github.com>
2018-10-23 20:48:01 -0300

    Removing custom font.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -69,11 +69,3 @@
  '(org-enforce-todo-dependencies t)
  '(org-startup-truncated nil)
  '(show-paren-mode t))
-
-;; init.el ends here
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack" :foundry "SRC" :slant normal :weight normal :height 98 :width normal)))))
