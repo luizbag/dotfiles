8bb5bfaf6e254045ddafedf62da10ed7816d88ae
Luiz Gavioli <428691+luizbag@users.noreply.github.com>
2022-02-21 19:46:04 +0000

    Added JetBrains Mono

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -113,4 +113,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 140 :width normal)))))
+ '(default ((t (:family "JetBrains Mono" :foundry "PfEd" :slant normal :weight normal :height 140 :width normal)))))
